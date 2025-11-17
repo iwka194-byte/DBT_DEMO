0f8c924b24cd65fbd1615769283fccf0c96764b3
ikurowsk@cisco.com <ikurowsk@cisco.com>
2025-11-17 16:35:30 +0000

    correction of the data to match 2018

--- models/station_dimension.sql
+++ models/station_dimension.sql
@@ -4,7 +4,7 @@ START_STATIO_ID as station_id,
 START_STATION_NAME as station_name,
 START_LAT as station_lat,
 START_LNG as station_lng
-from {{ source('demo', 'bike') }}
+from {{ ref('stg_bike') }}
 where RIDE_ID <> 'ride_id'
 )
 
